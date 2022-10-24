8abf12fe419fa43a19b2c5378046a872c02a5ea2
MarilenaCretu <marilenac2001@gmail.com>
2022-10-24 21:15:33 +0300

    Commit

--- src/main/java/Tema13/Author.java
+++ src/main/java/Tema13/Author.java
@@ -1,43 +1,6 @@
 package Tema13;
 
 public class Author {
-         int id;
-         int urmatorul_id;
-    //int getGetUrmatorul_id;
-         int author;
-    String quote;
-    //urmatorul_id_autor();
-//        public int urmatorul_id_autor() {
-//            urmatorul_id ++;
-//            return urmatorul_id;
-//        }
-    //int getUrmatorul_id;
-
-    public int getUrmatorul_id() {
-        urmatorul_id++;
-
-        return urmatorul_id;
-    }
-//    private static String authorToLine(Author author) {
-//        return String.format("%d ", author.getUrmatorul_id());
-//    }
-//    Author(){
-//        //id = Author:: urmatorul_id++;
-//        id = getUrmatorul_id();
-//    }
-    //int getUrmatorul_id;
 
 
-//    public String urmatorul_id_autor(int urmatorul_id) {
-//        urmatorul_id ++;
-//        //return urmatorul_id;
-//        System.out.println();
-//    }
-
-
-//        class Autor() {
-//            id = Autor:: urmatorul_id ++; //or, id = urmatorul_id_utilizator();
-//        }
-    //}
-    //int Autor :: urmatorul_id = 0;
 }
--- src/main/java/Tema13/Main.java
+++ src/main/java/Tema13/Main.java
@@ -3,38 +3,27 @@ package Tema13;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         readFileBuffered();
-//
+
     }
-//Quote a1=new Q();
-//    id = Autor:: urmatorul_id ++;
-//
-//        for (Author author : autors) {
-//            System.out.println("Animal: " + animal.getName() + " merge cu " + animal.getWalk() + ", face " + animal.getTalk() + " si mananca " + animal.getEat());
-//        }
 
     private static void readFileBuffered() throws IOException {
         System.out.println("Buffered read:");
         BufferedReader bufferedReader = new BufferedReader(new FileReader(("Fisiere/quotes.txt")));
-        int id;
+        int id = 0;
         String line;
         while ((line = bufferedReader.readLine()) != null) {
+            id++;
+            System.out.println(id + " " + line);
 
-            Author author=new Author();
-            //Author.urmatorul_id_autor();
-            //int urmatorul_id_autor=author.urmatorul_id;
-            //System.out.println(line+author.urmatorul_id);
-            //System.out.println(author.urmatorul_id+line);
-            //int urmatorul_id = 0;
-            //author.getUrmatorul_id();
-
-            System.out.println(author.getUrmatorul_id()+" "+line);
-            //System.out.println();
-
-            }
+            String[] tokens = line.split(Pattern.quote("~"));
+            //System.out.println(id + " " + tokens[0]);
+            //System.out.println(id + " " + tokens[1]);
         }
+    }
 
 }
