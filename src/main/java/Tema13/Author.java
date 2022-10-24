5a1b4dbaa85e273c2f679e1d3e1e5da2508f3905
MarilenaCretu <marilenac2001@gmail.com>
2022-10-24 21:22:54 +0300

    Commit

--- src/main/java/Tema13/Author.java
+++ src/main/java/Tema13/Author.java
@@ -1,6 +1,19 @@
 package Tema13;
 
 public class Author {
-
-
+    String author;
+    String quote;
+    String favorite;
+    public String getAuthorget(){
+        return author;
+    }
+    public String getQuote(){
+        return quote;
+    }
+    public void setFavorite(){
+        this.favorite = favorite;
+    }
+    public String getFavorite(){
+        return favorite;
+    }
 }
